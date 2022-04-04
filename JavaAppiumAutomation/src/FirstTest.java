63abd46c388874fdc8a2f8f369d152eaff52f929
kristina-berezovskaya <tina.kuznetsova@gmail.com>
2022-04-05 00:23:57 +0400

    Lesson2_Homework

---
+++ JavaAppiumAutomation/src/FirstTest.java
@@ -0,0 +1,41 @@
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import  io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import java.net.URL;
+
+public class FirstTest {
+
+    private AppiumDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "AndroidTestDevice");
+        capabilities.setCapability("platformVersion", "8.0");
+        capabilities.setCapability("automationName", "Appium");
+        capabilities.setCapability("appPackage", "org.wikipedia");
+        capabilities.setCapability("appActivity", ".main.MainActivity");
+        capabilities.setCapability("app", "C:\\Users\\Chris\\Desktop\\Lesson2\\JavaAppiumAutomation\\apks\\org.wikipedia_50397_apps.evozi.com.apk");
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+
+    @After
+    public void tearDown()
+    {
+        driver.quit();
+    }
+
+    @Test
+    public void firstTest()
+    {
+        System.out.println("First test run");
+    }
+
+}
